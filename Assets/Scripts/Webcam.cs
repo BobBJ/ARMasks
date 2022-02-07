e39df93316497ccabb10ff550f427ed07eedeb01
Boran Ben Jemia <boran.ben-jemia@etu-lyon1.fr>
2022-02-07 18:22:11 +0100

    Initialisation du projet avec le .gitignore et le travail qu'on a déja fait avec la webcam

---
+++ Assets/Scripts/Webcam.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Webcam : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        WebCamTexture webcamTexture = new WebCamTexture();
+        Debug.Log(webcamTexture.deviceName);
+        Renderer renderer = GetComponent<Renderer>();
+        renderer.material.mainTexture = webcamTexture;
+        webcamTexture.Stop();
+        webcamTexture.Play();
+    }
+}
